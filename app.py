e3e1f8deaaa83fd267d0c90210f01fc75709ab3f
Akash <akashgram0103@gmail.com>
2025-11-07 09:52:45 +0530

    Updated file

--- app.py
+++ app.py
@@ -144,14 +144,14 @@ if file is not None:
           x = st.selectbox("X axis",columns,width=300)
           plt.xlabel(x)
           bins = int(st.number_input("Enter no of Bins",max_value=20,value=6,width=300))
-          color = st.color_picker("Graph colour",width=300)
+          color = st.color_picker("Graph colour",width=300,value="#1DB0D6")
           sns.histplot(data[x],bins=bins,color=color)
         else:
           x = st.selectbox("X axis",columns,width=300)
           plt.xlabel(x)
           y = st.selectbox("Y axis",columns,width=300,key="y")   
           plt.ylabel(y)
-          color = st.color_picker("Graph colour",width=300)
+          color = st.color_picker("Graph colour",width=300,value="#1DB0D6")
         #----------------------------SCATTER PLOT----------------------------
         if plot == "Scatter":
           sns.scatterplot(data=data,x=data[x],y=data[y],color=color)
